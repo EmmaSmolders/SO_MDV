50c3a4ae9e25454eca8f986e3346826f6cd79059
EmmaSmolders <157585731+EmmaSmolders@users.noreply.github.com>
2026-06-04 11:17:14 +0200

    Update APE.py

--- Program/APE.py
+++ Program/APE.py
@@ -5,6 +5,7 @@ import time
 import glob, os
 import math
 import netCDF4 as netcdf
+import gsw
 
 #Making pathway to folder with all data
 directory_data	= '/projects/0/prace_imau/prace_2013081679/pop/tx0.1v2/pop.B2000.tx0.1v2.qe_hosing.001/tavg/'
@@ -34,25 +35,25 @@ def ReadinData(filename, layer_avail = False, volume_norm = False):
 	#Use negative longitudes for 180W - 0W
 	lon[lon>180]	= lon[lon>180] - 360.0
 
-	#Select region (SO30 or WGKP region)
-	lon_min_index	= (fabs(lon - -35)).argmin()
-	lon_max_index	= (fabs(lon - 80)).argmin() + 1
-	lat_min_index	= (fabs(lat - -90)).argmin()
-	lat_max_index	= (fabs(lat - -50)).argmin() + 1
-	
-	lon		= lon[lon_min_index:lon_max_index]
-	lat		= lat[lat_min_index:lat_max_index]
-	depth_grid	= depth_grid[lat_min_index:lat_max_index, lon_min_index:lon_max_index]
-	area		= area[lat_min_index:lat_max_index, lon_min_index:lon_max_index]
-	
-	#Select region (SO30 or WGKP region)
+	#Select region (WGKP region)
+	#lon_min_index	= (fabs(lon - -35)).argmin()
+	#lon_max_index	= (fabs(lon - 80)).argmin() + 1
 	#lat_min_index	= (fabs(lat - -90)).argmin()
-	#lat_max_index	= (fabs(lat - -30)).argmin() + 1
+	#lat_max_index	= (fabs(lat - -50)).argmin() + 1
 	
 	#lon		= lon[lon_min_index:lon_max_index]
 	#lat		= lat[lat_min_index:lat_max_index]
-	#depth_grid	= depth_grid[lat_min_index:lat_max_index, :]
-	#area		= area[lat_min_index:lat_max_index, :]
+	#depth_grid	= depth_grid[lat_min_index:lat_max_index, lon_min_index:lon_max_index]
+	#area		= area[lat_min_index:lat_max_index, lon_min_index:lon_max_index]
+	
+	#Select region (SO30 region)
+	lat_min_index	= (fabs(lat - -90)).argmin()
+	lat_max_index	= (fabs(lat - -30)).argmin() + 1
+	
+	#lon		= lon[lon_min_index:lon_max_index]
+	lat		= lat[lat_min_index:lat_max_index]
+	depth_grid	= depth_grid[lat_min_index:lat_max_index, :]
+	area		= area[lat_min_index:lat_max_index, :]
 	
 	#print(lon)
 	#print(lat)
@@ -61,15 +62,32 @@ def ReadinData(filename, layer_avail = False, volume_norm = False):
 		
 	fh = netcdf.Dataset(filename, 'r')
 
-	PD 		= fh.variables['PD'][:, lat_min_index:lat_max_index, lon_min_index:lon_max_index]*1000 	#Potential density (kg/m^3)
+	#PD 		= fh.variables['PD'][:, lat_min_index:lat_max_index, lon_min_index:lon_max_index]*1000 	#Potential density (kg/m^3)
+	#PD 		= fh.variables['PD'][:, lat_min_index:lat_max_index, :]*1000 	#Potential density (kg/m^3)
+	temp 		= fh.variables['TEMP'][:, lat_min_index:lat_max_index, :] 	#Potential temperature (degC)
+	salt 		= fh.variables['SALT'][:, lat_min_index:lat_max_index, :]*1000 	#Salinity (g/kg)
 	
 	fh.close()
 	
-	print(np.where(PD < 0))
+	#Mask the area
+	temp		= ma.masked_where(salt <= 0, temp)	
+	salt		= ma.masked_where(salt <= 0, salt)
+	
+	PD 		= ma.masked_all((len(depth), len(lat), len(lon)))
 	
 	for depth_i in range(len(depth)):
+		#print(depth_i)
+		#First determine the conservative temperature from the potential temperature
+		temp_CT		= gsw.CT_from_pt(salt[depth_i], temp[depth_i])
+		
+		#Get the potential density
+		PD[depth_i]		= gsw.sigma0(salt[depth_i], temp_CT)+1000.0
+	
+	#print(np.where(PD < 0))
+	
+	#for depth_i in range(len(depth)):
 		#Mask all the field at the topography
-		PD[depth_i]	= ma.masked_where(depth_grid <= depth_top[depth_i], PD[depth_i])
+	#	PD[depth_i]	= ma.masked_where(depth_grid <= depth_top[depth_i], PD[depth_i])
 
 	#------------------------------------------------------------------------------
 	#Make volume on T-grid
@@ -109,9 +127,9 @@ def ReadinData(filename, layer_avail = False, volume_norm = False):
 g = 9.81  # Gravitational constant
 window = 5 # Moving average window
 
-#First SOM cycle (model year 63-114), second (324-378) or last SOM cycle (500-600)
-year_start	= 324
-year_end	= 378
+#First SOM cycle (model year 63-114), second (410-480) or last SOM cycle (500-600)
+year_start	= 350
+year_end	= 600
 
 files = []
 
@@ -129,48 +147,47 @@ files	= files[(year_start-1)*12:year_end*12]
 print(files[0])
 print(files[-1])
 
+#-----------------------------------------------------------------------------------------
+
 # Read initial data
 lat, lon, depth, area, layer, volume, PD = ReadinData(files[0])
 
-# Initialize arrays
-time_year = np.arange(year_start, year_end+1)
+# Read in reference state and take time average
+fh = netcdf.Dataset(directory + 'Ocean/PD_global_final.nc', 'r')
+
+PD_global = fh.variables['PD'][:] 	#monthly global area-averaged potential density (kg/m^3)
+
+#Take yearly-averaged rho_ref over first 50 years (or over the SOM cycle?)
+PD_global = PD_global[0:50*12,:]
+#PD_global = PD_global[(year_start - 1)*12:year_end*12,:]
 
 # Precompute month weights
 month_days = np.array([31., 28., 31., 30., 31., 30., 31., 31., 30., 31., 30., 31.])
 month_weights = month_days / np.sum(month_days)
+month_weights_tiled = np.tile(month_weights, 50)  # Repeat weights for 50 years
+#month_weights_tiled = np.tile(month_weights, year_end - year_start + 1) 
 
-# Process each year
-for year_i, year in enumerate(time_year):
-    PD_year = ma.masked_all((12, len(depth), len(lat), len(lon)))
-    for month_i in range(12):
-        filename = files[year_i * 12 + month_i]
-        _, _, _, _, _, _, PD_year[month_i] = ReadinData(filename)
+# Compute weighted mean for the years
+rho_ref = np.sum(PD_global * month_weights_tiled[:, np.newaxis], axis=0) / np.sum(month_weights_tiled)
 
-    # Compute weighted mean for the year
-    PD_all = np.sum(PD_year * month_weights[:, np.newaxis, np.newaxis, np.newaxis], axis=0)
-
-    if year_i == 0:
-        PD_mean = np.copy(PD_all) / len(time_year) 
-
-    else:
-        PD_mean += np.copy(PD_all) / len(time_year) 
-            
-# Compute reference state
-#PD_mean = np.mean(PD_all, axis=0)
-
-#del PD_all
-
-rho_ref = np.sum(PD_mean * area[np.newaxis, :, :], axis=(1, 2)) / np.sum(area)
-
-del PD_mean
+#-----------------------------------------------------------------------------------------
 
 # Compute vertical gradient of rho_ref
 n0 = np.zeros(len(depth))
-n0[1:-1] = (rho_ref[:-2] - rho_ref[2:]) / (2.0 * layer[1:-1])
-n0[0] = (rho_ref[0] - rho_ref[1]) / layer[0]
-n0[-1] = (rho_ref[-2] - rho_ref[-1]) / layer[-1]
-
-del rho_ref
+for depth_i in range(len(depth)):
+	#print(depth_i)
+	#print(layer[depth_i])
+	#print(dens[depth_i])
+	#print(dens[depth_i - 1])
+	#print(dens[depth_i + 1])
+	#if ma.is_masked(dens[depth_i]):
+	#	continue
+	if depth_i == 0:
+		n0[depth_i] = (rho_ref[depth_i] - rho_ref[depth_i+1])/layer[0]
+	elif depth_i == len(depth) - 1:
+		n0[depth_i] = (rho_ref[depth_i - 1] - rho_ref[depth_i])/layer[depth_i]
+	else:
+		n0[depth_i] = (rho_ref[depth_i-1] - rho_ref[depth_i+1])/(0.5*layer[depth_i-1] + layer[depth_i] + 0.5*layer[depth_i+1])
 
 print('Data is written to file')
 fh = netcdf.Dataset(directory+'Ocean/n0_year_'+str(year_start)+'-'+str(year_end)+'_SO30.nc', 'w')
@@ -187,7 +204,7 @@ fh.createVariable('n0', float, ('depth'), zlib=True)
 fh.variables['depth'].long_name 	= 'Depth'
 fh.variables['lat'].long_name		= 'Latitudes'
 fh.variables['lon'].long_name		= 'Longitudes'
-fh.variables['n0'].long_name 		= 'Inverse vertical density gradient'
+fh.variables['n0'].long_name 		= 'Vertical density gradient'
 
 fh.variables['depth'].units 	= 'm'
 fh.variables['lat'].units 	= 'Degrees N'
@@ -202,24 +219,7 @@ fh.variables['n0'][:] 		= n0
 
 fh.close()
 
-#Read in mean PD
-#fh = netcdf.Dataset(directory+'Ocean/PD_mean_year_'+str(year_start)+'-'+str(year_end)+'_SO30.nc','r')
-
-#PD_mean = fh.variables['PD_mean'][:] #time mean PD (shape [depth, lat, lon])
-
-#fh.close()
-
-#rho_ref = np.sum(PD_mean * area[np.newaxis, :, :], axis=(1, 2)) / np.sum(area)
-
-#del PD_mean
-
-# Compute vertical gradient of rho_ref
-#n0 = np.zeros(len(depth))
-#n0[1:-1] = (rho_ref[:-2] - rho_ref[2:]) / (2.0 * layer[1:-1])
-#n0[0] = (rho_ref[0] - rho_ref[1]) / layer[0]
-#n0[-1] = (rho_ref[-2] - rho_ref[-1]) / layer[-1]
-
-#del rho_ref
+#-----------------------------------------------------------------------------------------
 
 # Initialize arrays
 time_year 	= np.arange(year_start, year_end+1)
@@ -247,13 +247,14 @@ for year_i in range(len(time_year)):
 		#print(f"Year: {year_i}, Month: {month_i}, PDW_all index: {pdw_index}")
 		
 		#Save monthly PD*PD in window
-		PDPD_all[pdw_index, :,:,:] = PD_month**2
+		PD_month = PD_month - rho_ref[:, np.newaxis, np.newaxis] #rho_star
+		PDPD_all[pdw_index, :,:,:] = (PD_month**2) * month_weights[month_i] #rho_star^2
         	
 	#Start calculating energetics when year_i has at least 5 years
 	if year_i >= window - 1:
 	
 		#Compute mean values in window
-		PD2_mean	= np.mean(PDPD_all, axis = 0)
+		PD2_mean	= np.sum(PDPD_all, axis = 0) / window #divide by windowlength because we have every months 5 times
 		
 		APE_year = -0.5 * g * (1 / n0[:, np.newaxis, np.newaxis]) * PD2_mean
 		
@@ -264,7 +265,7 @@ for year_i in range(len(time_year)):
 	#-----------------------------------------------------------------------------------------
 
 	print('Data is written to file')
-	fh = netcdf.Dataset(directory+'Ocean/APE_year_'+str(year_start)+'-'+str(year_end)+'_window_'+str(window)+'_volume_integrated_WGKP.nc', 'w')
+	fh = netcdf.Dataset(directory+'Ocean/APE_year_'+str(year_start)+'-'+str(year_end)+'_window_'+str(window)+'_volume_integrated_SO30_TEST_rhoref_0-50_TEST_goedegsw.nc', 'w')
 
 	fh.createDimension('time', len(time_year) - window + 1)
 	fh.createDimension('lat', len(lat))
